86992dbec05a76d056a38e351de9965af09993de
tracer-nju <tracer@nju.edu.cn>
2026-02-13 18:50:00 +0800

    Linux yl-Legion-Y9000X 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:50:00 up  4:05,  1 user,  load average: 0.00, 0.00, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,5 +1,23 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+
+typedef struct process_info {
+  char* name;
+  pid_t pid;
+  pid_t ppid;
+  int child_thread_num;
+  struct process_info** child_threads; // pointer to the child threads array 
+} process_info;
 
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
@@ -7,5 +25,53 @@ int main(int argc, char *argv[]) {
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
+
+  // command line arguments collection
+  bool show_pid = false, sort = false, version = false;
+  int opt;
+  while (opt = getopt(argc, argv, "pnV") != -1) {
+    switch (opt) {
+      case 'p':
+        show_pid = true;
+        break;
+      case 'n':
+        sort = true;
+        break;
+      case 'V':
+        version = true;
+        break;
+      case '?':
+        fprintf(stderr, "Usage: %s [-V] [-n] [-p]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+  }
+  // if only print version, just print it and early return
+  if (version) {
+    fprintf(stderr, "pstree (for NJU-OS) 1.0\nCopyright (C) 2026 yl\n");
+    exit(EXIT_SUCCESS);
+  }
+  // collect process info
+  DIR *dir;
+  struct dirent *entry;
+
+  // Open the directory
+  dir = opendir("/proc");
+  if (dir == NULL) {
+    perror("opendir");
+    exit(EXIT_FAILURE);
+  }
+
+  // Read directory entries
+  while ((entry = readdir(dir)) != NULL) {
+    // Check if the entry is a directory and not "." or ".."
+    if (entry->d_type == DT_DIR) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+          printf("%s\n", entry->d_name);
+        }
+    }
+  }
+
+  // Close the directory
+  closedir(dir);
   return 0;
 }
